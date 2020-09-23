76ec99d6ef6497cd8be055c2a53c6dab627758f8
siddesh <vinay22422@gmail.com>
2020-09-23 10:17:48 +0530

    1stprogram.js

---
+++ 1stprogram.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h2>
+My first java scriptcode
+</h2>
+<p id="demo">
+
+</p>
+<button type="button" onClick="document.getElementById('demo').innerHTML=Date()">Click here for time
+</button>
+</body>
+</html>
